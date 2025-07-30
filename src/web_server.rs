f1ff72776d2c0eaec330ffe0e4cfc472dfe01d35
zbz <nnzbz@qq.com>
2025-07-30 19:06:55 +0800

    + 添加健康检查接口

--- src/web_server.rs
+++ src/web_server.rs
@@ -3,9 +3,13 @@ use actix_web::dev::Server;
 use actix_web::web::Data;
 use actix_web::{App, HttpResponse, HttpServer, Responder, get};
 
-#[get("/hello")]
-async fn greet(targets: Data<Targets>) -> impl Responder {
+#[get("/metrics")]
+async fn metrics(targets: Data<Targets>) -> impl Responder {
     HttpResponse::Ok().json(targets.get_all())
+}
+#[get("/health")]
+async fn health() -> impl Responder {
+    "Ok"
 }
 
 pub struct WebServer {
@@ -15,12 +19,17 @@ pub struct WebServer {
 impl WebServer {
     pub fn new(port: u16, targets: Targets) -> Self {
         let data = Data::new(targets);
-        let server = HttpServer::new(move || App::new().app_data(data.clone()).service(greet))
-            .bind(("127.0.0.1", port))
-            .unwrap()
-            .bind(("::1", port))
-            .unwrap()
-            .run();
+        let server = HttpServer::new(move || {
+            App::new()
+                .app_data(data.clone())
+                .service(metrics)
+                .service(health)
+        })
+        .bind(("127.0.0.1", port))
+        .unwrap()
+        .bind(("::1", port))
+        .unwrap()
+        .run();
         Self { server }
     }
 
