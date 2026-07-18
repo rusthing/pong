8ce9fea33f9b16cf5c9595e84622b2e4d6f25164
nnzbz <nnzbz@qq.com>
2026-07-18 13:09:00 +0800

    * 更新build.rs

--- build.rs
+++ build.rs
@@ -11,24 +11,66 @@ use std::path::Path;
 /// - 当文件复制失败时会panic
 fn main() {
     // println!("cargo:warning=🔍 正在运行 build.rs");
-    // 获取输出目录路径
+    // 获取项目根目录
+    let project_root = env!("CARGO_MANIFEST_DIR");
+    // 获取编译脚本文件的输出目录路径
     let out_dir = std::env::var("OUT_DIR").unwrap();
+    // 构造目标文件路径，通过向上回溯OUT_DIR的父级目录来定位
+    let dest_dir_path = Path::new(&out_dir)
+        .ancestors()
+        .nth(3)
+        .expect("Failed to get parent directory");
+    // println!(
+    //     "cargo:warning=PROJECT_ROOT: {project_root:?} OUT_DIR: {out_dir} dest_path: {dest_dir_path:?}"
+    // );
 
     // 复制应用的配置文件到输出目录
-    let config_file_name = env!("CARGO_PKG_NAME");
-    copy_file(&out_dir, config_file_name, "toml");
-    copy_file(&out_dir, config_file_name, "yml");
-    copy_file(&out_dir, config_file_name, "json");
-    copy_file(&out_dir, config_file_name, "ini");
-    copy_file(&out_dir, config_file_name, "ron");
+    let file_name = env!("CARGO_PKG_NAME");
+    copy_file(project_root, file_name, "toml", dest_dir_path);
+    copy_file(project_root, file_name, "yml", dest_dir_path);
+    copy_file(project_root, file_name, "json", dest_dir_path);
+    copy_file(project_root, file_name, "ini", dest_dir_path);
+    copy_file(project_root, file_name, "ron", dest_dir_path);
 
     // 复制日志的配置文件到输出目录
-    let config_file_name = "log";
-    copy_file(&out_dir, config_file_name, "toml");
-    copy_file(&out_dir, config_file_name, "yml");
-    copy_file(&out_dir, config_file_name, "json");
-    copy_file(&out_dir, config_file_name, "ini");
-    copy_file(&out_dir, config_file_name, "ron");
+    let file_name = "log";
+    copy_file(project_root, file_name, "toml", dest_dir_path);
+    copy_file(project_root, file_name, "yml", dest_dir_path);
+    copy_file(project_root, file_name, "json", dest_dir_path);
+    copy_file(project_root, file_name, "ini", dest_dir_path);
+    copy_file(project_root, file_name, "ron", dest_dir_path);
+}
+
+fn copy_dir(project_root: &str, src_dir: &str, dest_dir_path: &Path) {
+    let src_dir_path = Path::new(project_root).join(src_dir);
+    if !src_dir_path.exists() || !src_dir_path.is_dir() {
+        return;
+    }
+
+    let dest_dir = dest_dir_path.join(src_dir);
+    copy_dir_recursive(&src_dir_path, &dest_dir);
+}
+
+fn copy_dir_recursive(src_path: &Path, dest_path: &Path) {
+    if src_path.is_dir() {
+        fs::create_dir_all(dest_path).expect("Failed to create directory");
+
+        for entry in fs::read_dir(src_path).expect("Failed to read directory") {
+            let entry = entry.expect("Failed to read directory entry");
+            let entry_path = entry.path();
+            let entry_name = entry.file_name();
+            let dest_entry_path = dest_path.join(entry_name);
+
+            if entry_path.is_dir() {
+                copy_dir_recursive(&entry_path, &dest_entry_path);
+            } else if entry_path.is_file() {
+                // 告诉 Cargo 当文件变化时重新运行 build.rs
+                println!("cargo:rerun-if-changed={}", entry_path.display());
+                // 复制文件到目的地
+                fs::copy(&entry_path, &dest_entry_path).expect("Failed to copy file");
+            }
+        }
+    }
 }
 
 /// 复制指定扩展名的配置文件到输出目录
@@ -53,30 +95,20 @@ fn main() {
 /// - 当无法访问环境变量时
 /// - 当路径操作失败时
 /// - 当文件复制失败时
-fn copy_file(out_dir: &str, config_file_name: &str, file_ext: &str) {
+fn copy_file(project_root: &str, src_file_name: &str, src_file_ext: &str, dest_path: &Path) {
     // 获取源配置文件路径
-    let config_file_name = format!("{config_file_name}.{file_ext}");
+    let src_file_name = format!("{src_file_name}.{src_file_ext}");
 
-    let project_root = env!("CARGO_MANIFEST_DIR");
-    let config_file_path = Path::new(project_root).join(&config_file_name);
+    let src_file_path = Path::new(project_root).join(&src_file_name);
 
-    // 构造目标文件路径，通过向上回溯OUT_DIR的父级目录来定位
-    let dest_path = Path::new(&out_dir)
-        .ancestors()
-        .nth(3)
-        .expect("Failed to get parent directory")
-        .join(&config_file_name);
+    // 如果源文件存在，则执行复制操作
+    if src_file_path.exists() {
+        let dest_path = dest_path.join(&src_file_name);
+        // println!("cargo:warning=copy {:?} to {:?}", src_file_path, dest_path);
 
-    // 如果源配置文件存在，则执行复制操作
-    if config_file_path.exists() {
-        // println!(
-        //     "cargo:warning=copy {:?} to {:?}",
-        //     config_file_path, dest_path
-        // );
-
-        // 告诉 Cargo 当配置文件变化时重新运行 build.rs
-        println!("cargo:rerun-if-changed={config_file_name}");
+        // 告诉 Cargo 当文件变化时重新运行 build.rs
+        println!("cargo:rerun-if-changed={src_file_name}");
         // 复制文件到目的地
-        fs::copy(config_file_path, dest_path).expect("Failed to copy app file");
+        fs::copy(src_file_path, dest_path).expect("Failed to copy app file");
     }
 }
